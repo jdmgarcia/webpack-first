13627f194b711f161cf209a0719d2365186f0c7b
Juan Garcia <jdmgarciamoreno@gmail.com>
2017-04-05 15:45:51 +0100

    Add my app with webpack

---
+++ app/index.js
@@ -0,0 +1,11 @@
+//import Logger
+import Logger from './logger.js';
+
+//import CSS
+require('../styles.scss');
+
+//update content of greeting element when DOM has been loaded
+document.addEventListener('DOMContentLoaded', () => {
+  document.getElementById('greeting').innerHTML = "Hello my friend!";
+  Logger("Greeting has been updated");
+})
---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+//  webpack.config.js
+
+var webpack = require('webpack');
+
+module.exports = {
+  entry: './app/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: __dirname
+  },
+  devtool: 'eval-source-map',
+  module: {
+    loaders: [
+      {test:/\.js$/, loader:'babel-loader', exclude: /node_modules/, query: { presets: ['es2015'] } },
+      {test:/\.scss$/, loader:'style-loader!css-loader!sass-loader', exclude: /node_modules/}
+    ]
+  },
+  plugins: [
+    new webpack.optimize.UglifyJsPlugin()
+  ]
+};
